28db7cd0cf1dcf950a245feeb8a1d54d52455827
ghazi-naceur <ghazi.ennacer@gmail.com>
2024-01-21 13:01:26 +0100

    Header

--- app/src/main/scala/rop/jobsboard/components/Header.scala
+++ app/src/main/scala/rop/jobsboard/components/Header.scala
@@ -11,12 +11,70 @@ import scala.scalajs.js.annotation.*
 
 object Header {
 
+//                    <li class="nav-item">
+//                      <a class="nav-link jvm-item Home active-item" href="index.html">Home</a>
+//
+//                    </li>
+//                    <li class="nav-item">
+//                      <a class="nav-link jvm-item job" href="jobs.html">Jobs</a>
+//                    </li>
+//                    <li class="nav-item">
+//                      <a class="nav-link jvm-item contect-us" href="Contact-us.html">Contact Us</a>
+//                    </li>
+//                    <li class="nav-item">
+//                      <a class="nav-link jvm-iteme" href="#">|</a>
+//                    </li>
+//                    <li class="nav-item">
+//                      <a class="nav-link jvm-item post-a-job" href="Post-a-Job.html">Post A Job</a>
+//                    </li>
+//                    <!-- start-button   -->
+//
+//                    <li class="nav-item">
+//                      <a class="nav-link " href="signin.html">
+//                        <button class="btn-register" type="text">Register</button>
+//                      </a>
+//                    </li>
+//                    <li class="nav-item">
+//                      <a class="nav-link " href="login.html">
+//                        <button class="btn-logout" type="text">Login</button>
+//                      </a>
+//                    </li>
+//
+//                    <!-- End-button   -->
+//
+//                  </ul>
+//                </div>
+//              </div>
+//            </nav>
+//          </div>
+//        </div>
+//      </div>
+
   def view() = {
-    div(`class` := "header-container")(
-      renderLogo(),
-      div(`class` := "header-nav")(
-        ul(`class` := "header-links")(
-          renderNavLinks()
+    div(`class` := "container-fluid p-0")(
+      div(`class` := "jvm-nav")(
+        div(`class` := "container")(
+          nav(`class` := "navbar navbar-expand-lg navbar-light JVM-nav")(
+            div(`class` := "container")(
+              renderLogo(),
+              button(
+                `class` := "navbar-toggler",
+                `type`  := "button",
+                attribute("data-bs-toggle", "collapse"),
+                attribute("data-bs-target", "#navbarNav"),
+                attribute("aria-controls", "navbarNav"),
+                attribute("aria-expanded", "false"),
+                attribute("aria-label", "Toggle navigation")
+              )(
+                span(`class` := "navbar-toggler-icon")()
+              ),
+              div(`class` := "collapse navbar-collapse", id := "navbarNav")(
+                ul(`class` := "navbar-nav ms-auto menu align-center expanded text-center SMN_effect-3")(
+                  renderNavLinks()
+                )
+              )
+            )
+          )
         )
       )
     )
@@ -38,40 +96,39 @@ object Header {
     'renderLogo'.
    */
   private def renderLogo() = {
-    li(`class` := "nav-item")(
-      a(
-        href := "/",
-        onEvent(
-          "click",
-          e => {
-            e.preventDefault()
-            Router.ChangeLocation("/")
-          }
-        )
-      )(
-        img(
-          `class` := "home-logo",
-          src     := logoImage,
-          alt     := "Konoha Corp"
-        )
+    a(
+      href    := "/",
+      `class` := "navbar-brand",
+      onEvent(
+        "click",
+        e => {
+          e.preventDefault()
+          Router.ChangeLocation("/")
+        }
+      )
+    )(
+      img(
+        `class` := "home-logo",
+        src     := logoImage,
+        alt     := "Konoha Corp"
       )
     )
   }
 
   private def renderNavLinks(): List[Html[App.Msg]] = {
     val constantLinks = List(
-      Anchors.renderSimpleNavLink("Jobs", JOBS),
-      Anchors.renderSimpleNavLink("Post job", POST_JOB)
+      renderSimpleNavLink("Jobs", JOBS),
+      renderSimpleNavLink("Post job", POST_JOB)
     )
 
     val unauthedLinks = List(
-      Anchors.renderSimpleNavLink("Login", LOGIN),
-      Anchors.renderSimpleNavLink("Sign up", SIGNUP)
+      renderSimpleNavLink("Login", LOGIN),
+      renderSimpleNavLink("Sign up", SIGNUP)
     )
 
     val authedLinks = List(
-      Anchors.renderSimpleNavLink("Profile", PROFILE),
-      Anchors.renderNavLink("Log out", HASH)(_ => Session.Logout)
+      renderSimpleNavLink("Profile", PROFILE),
+      renderNavLink("Log out", HASH)(_ => Session.Logout)
     )
 
     constantLinks ++ (
@@ -79,4 +136,13 @@ object Header {
       else unauthedLinks
     )
   }
+
+  private def renderSimpleNavLink(text: String, location: String) =
+    renderNavLink(text, location)(Router.ChangeLocation(_))
+
+  private def renderNavLink(text: String, location: String)(location2msg: String => App.Msg) =
+    li(`class` := "nav-item")(
+      Anchors.renderNavLink(text, location, "nav-link jvm-item Home active-item")(location2msg)
+    )
+
 }
